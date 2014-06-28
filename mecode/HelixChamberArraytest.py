c14b3d0794fe63af1e9a58f01d5e9e53d215aa10
Dan Fitzgerald <danfitz7@hotmail.com>
2014-06-28 13:01:51 -0400

    Finished Array
    NxM Helix Array of varying radius and number of spirals

--- mecode/HelixChamberArraytest.py
+++ mecode/HelixChamberArraytest.py
@@ -172,22 +172,22 @@ com_port = 9
 z_axis = "z"
 
 #spiral parameters
-n_points = 100     #number of points to discretize the helix in to
-radius = 2.5
+n_points = 1000     #number of points to discretize the helix in to
+start_radius = 0.5
 n_spirals = 36
 factor=1         # discretization factor. Influences the effect of the arctan scaling of discretization.
 
 #other dimensions
-stem_length = 1 # connection to needle insertion
-pad_length = 2 # needle insertion
-lift_height = 10
+stem_length = 1    # connection to needle insertion
+pad_length = 2     # needle insertion
+travel_height = mold_z_zero+10   # safe travel height (abs)
 
 #feeds and speeds
-pressure = 90 # line pressure to ink nozzle
-matrix_travel_speed = 1 # (in mm/s) travel in matrix speed
-stem_print_speed = 1 # (in mm/s) stem (needle insertion) print speed
-air_travel_speed = 20 # (in mms) air travel
-print_speed = 0.5 #in (mm/s) helix print speed
+pressure = 90             # (psi) line pressure to ink nozzle
+matrix_travel_speed = 1   # (in mm/s) travel in matrix speed
+stem_print_speed = 1      # (in mm/s) stem (needle insertion) print speed
+air_travel_speed = 20     # (in mms) air travel
+print_speed = 0.5         # (in mm/s) helix print speed
 
 g.write(
 """
@@ -205,18 +205,22 @@ VELOCITY ON ; interpolate velocities instead of starting and stopping
 g.set_pressure(com_port, pressure)
 
 #array parameters
-n_rows = 1
+n_rows = 4
 radius_decrease_per_row = 0.5
-n_cols = 1
-n_spirals_decrease_per_col = 5
+
+n_cols = 4
+n_spirals_decrease_per_col = 10
+
 spiral_sep_dist = 2 # separation between spiral edges
+max_radius = start_radius+radius_decrease_per_row*(n_rows-1)
+col_spacing = spiral_sep_dist + 2*(max_radius)
+center_height = mold_z_zero+mold_zero_offset+max_radius
 
-for col in range(n_cols):
-    radius=0.5+0.5*col
-
-    for row in range(n_rows):
-    
-        n_spirals = n_spirals/2
+for col in range(n_cols):             #loop through columns (right, posative in X)
+    n_spirals = n_spirals-n_spirals_decrease_per_col  #the number of spirals decreases across columns
+    y_counter=0
+    for row in range(n_rows): #loop through rows (down, negative in Y)
+        radius=start_radius+radius_decrease_per_row*row    # decrease radius each row 
 
         #make the spiral
         theta = np.linspace(-np.arctan(factor),np.arctan(factor), n_points)
@@ -228,7 +232,6 @@ for col in range(n_cols):
         y = radius*np.sin(theta_discretized) * np.sin(phi)
         z = -radius*np.cos(theta_discretized)
         
-        
         #points array    
         pts = np.vstack((x, y, z))
         pts = pts.T
@@ -237,12 +240,13 @@ for col in range(n_cols):
         g.absolute();
         g.write("ABSOLUTE")
         g.feed(matrix_travel_speed) # safe immersion speed
-        g.move(**{z_axis:(mold_z_zero+mold_zero_offset)})
+        base_height=center_height-radius
+        g.move(**{z_axis:(base_height)}) #safe height above bottom of mold
         g.relative();
         g.write("Incremental")
             
-        #start sphere
-        g.feed(print_speed) # set print speed
+        #make a sphere helix
+        g.feed(print_speed)         # set print speed
         g.toggle_pressure(com_port) # start extrusion
         relativePoints = pts[1:]-pts[:-1]
         for relPoint in relativePoints:
@@ -256,16 +260,20 @@ for col in range(n_cols):
         g.move(**{z_axis:pad_length})
             
         #g.feed(matrix_travel_speed)
-        g.move(**{z_axis:lift_height})
+        g.absolute()
+        g.abs_move(**{z_axis:travel_height})
+        g.relative()    
             
         #stop extrusion
         g.toggle_pressure(com_port)
         
-        #move down to next row
-        g.move(y=-2*radius-0.5-spiral_sep_dist)
+        #move down to next col
+        dist_to_next_row = 2*radius+radius_decrease_per_row+spiral_sep_dist
+        g.move(y=dist_to_next_row)
+        y_counter=y_counter+dist_to_next_row
         
-    #move back up in Y and over in x for the next col    
-    g.move(x=2*radius+0.5+spiral_sep_dist, y = n_rows*(0.5*2+spiral_sep_dist)-spiral_sep_dist)
+    #move back up in Y and over in x for the next col  
+    g.move(x=col_spacing, y=-y_counter)
     
 g.write(end_script_string)    
     
